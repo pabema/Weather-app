0bc906859ac21e1e05b68ca402c5c4496074117f
pabema <pablobellver.bm@gmail.com>
2024-08-27 01:01:01 +0200

    Display de la aplicacion y imagen dinamica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InfoWeatherComponent } from './components/info-weather/info-weather.component';
+import { WeatherForecastComponent } from './components/weather-forecast/weather-forecast.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InfoWeatherComponent],
+  imports: [RouterOutlet, InfoWeatherComponent, WeatherForecastComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/info-weather/info-weather.component.ts
+++ src/app/components/info-weather/info-weather.component.ts
@@ -16,26 +16,31 @@ export class InfoWeatherComponent implements OnInit {
   ubicacion: any = '';
   datos: any = [];
 
+  image_display: string = '';
+
   constructor(private req: RequestService) {}
 
   onSubmit() {
-    console.log(this.ubicacion);
-    let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${this.ubicacion}?unitGroup=metric&key=GU79KU3ZKLN4855LDJD4RV9CZ&contentType=json`;
-    this.req.getInfoWeather(url).then(
-      (response: any) => {
-        this.datos = response;
-        console.log(this.datos); // Aquí tendrás los datos recibidos de la API
-      },
-      (error: any) => {
-        console.error('Error al obtener los datos:', error);
-      }
-    );
+    // console.log(this.ubicacion);
+    // let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${this.ubicacion}?unitGroup=metric&key=GU79KU3ZKLN4855LDJD4RV9CZ&contentType=json`;
+    // this.req.getInfoWeather(url).then(
+    //   (response: any) => {
+    //     this.datos = response;
+    //     this.image_display = `assets/${response.currentConditions.icon}.svg`;
+    //     console.log(this.image_display);
+    //     console.log(this.datos); // Aquí tendrás los datos recibidos de la API
+    //   },
+    //   (error: any) => {
+    //     console.error('Error al obtener los datos:', error);
+    //   }
+    // );
   }
 
   ngOnInit(): void {
-    document.addEventListener('click', () => {
-      console.log(this.ubicacion);
-      console.log(this.datos.resolvedAddress);
-    });
+    // document.addEventListener('click', () => {
+    //   console.log(this.ubicacion);
+    //   console.log(this.datos.resolvedAddress);
+    //   console.log(this.image_display);
+    // });
   }
 }
